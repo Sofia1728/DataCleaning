3114336ca8c16dc5a591d8b32a7963bc80109d74
Sofia1728 <sofiavdovichenko11@gmail.com>
2025-01-13 21:56:48 +0300

    Update createDirt.py

--- createDirt.py
+++ createDirt.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 
-# Создаем "грязный" набор данных
+
 data = {
     'Name': ['Alice', 'Bob', 'Charlie', np.nan, 'Eve', 'Grace', 'Hannah', 'John', 'Mike', np.nan],
     'Age': [25, 30, np.nan, 22, 35, 40, 25, np.nan, 29, 33],
@@ -10,9 +10,8 @@ data = {
 
 df = pd.DataFrame(data)
 
-# Сохраняем в CSV, чтобы проверить, как это выглядит
 df.to_csv('dirty_data.csv', index=False)
 
-# Выводим набор данных
+
 print("Изначальные данные:")
 print(df)
